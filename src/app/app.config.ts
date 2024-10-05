0f07dd48c05875cc3180dca954b9305b06793283
Ed <djaziz12393@gmail.com>
2024-10-05 08:50:29 +0300

    init

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,17 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import {ApplicationConfig, provideZoneChangeDetection} from '@angular/core';
+import {provideRouter} from '@angular/router';
 
-import { routes } from './app.routes';
+import {routes} from './app.routes';
+import {getFirestore, provideFirestore} from '@angular/fire/firestore';
+import {initializeApp, provideFirebaseApp} from '@angular/fire/app';
+import {getStorage, provideStorage} from '@angular/fire/storage';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({eventCoalescing: true}),
+    provideRouter(routes),
+    provideFirebaseApp(() => initializeApp()),
+    provideFirestore(() => getFirestore()),
+    provideStorage(() => getStorage()),
+  ]
 };
