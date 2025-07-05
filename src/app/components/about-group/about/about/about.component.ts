d1e71ce504ba911b61e4264ff49f47f31963bcc0
EnaEd <djaziz12393@gmail.com>
2025-07-05 21:31:57 +0300

    Upgrade to Angular v19.2.14 and update dependencies

--- src/app/components/about-group/about/about/about.component.ts
+++ src/app/components/about-group/about/about/about.component.ts
@@ -2,11 +2,10 @@ import { Component } from '@angular/core';
 import {Router} from '@angular/router';
 
 @Component({
-  selector: 'app-about',
-  standalone: true,
-  imports: [],
-  templateUrl: './about.component.html',
-  styleUrl: './about.component.css'
+    selector: 'app-about',
+    imports: [],
+    templateUrl: './about.component.html',
+    styleUrl: './about.component.css'
 })
 export class AboutComponent {
 constructor(private router: Router) {
